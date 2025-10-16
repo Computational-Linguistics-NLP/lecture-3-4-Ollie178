513f46ad14ab8aa3b98da1c9443ca99cf641bddb
Ollie178 <ol.bredikhina@gmail.com>
2025-10-16 23:52:29 +0300

    Add files via upload

---
+++ Test3.4.py
@@ -0,0 +1,28 @@
+# Первый способ
+year = int(input("Введите год: "))
+if year % 400 == 0:
+    print("да")
+elif year % 100 == 0:
+    print("нет")
+elif year % 4 == 0:
+    print("да")
+else:
+    print("нет")
+
+
+
+# Второй способ
+year = int(input("Введите год: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("да")
+else:
+    print("нет")
+
+
+
+# Третий способ
+year = int(input("Введите год: "))
+result = "да" if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0) else "нет"
+print(result)
+
+
